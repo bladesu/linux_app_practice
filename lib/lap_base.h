9b569f2090f2db07fa3eca977626e986b5e2ed8c
BladeSu <bladesu@inquartik.com>
2020-04-22 15:38:19 +0800

    Introduce /lib/get_num.c, /lib/get_num.h as base library to parse number.

--- lib/lap_base.h
+++ lib/lap_base.h
@@ -9,8 +9,7 @@
 #include <errno.h>      /* Declares errno and defines error constants */
 #include <string.h>     /* Commonly used string-handling functions */
 #include "error_functions.h"  /* Declares our error-handling functions */
-//#include "get_num.h" 
-/* Declares our functions for handling numeric arguments (getInt(), getLong()) */
+#include "get_num.h"    /* Declares our functions for handling numeric arguments (getInt(), getLong()) */
                 
 
 typedef enum { FALSE, TRUE } Boolean;
