a4faaecf45e2eedcd95510aa1dc4d2157bf1da3d
BladeSu <bladesu@inquartik.com>
2020-04-28 21:44:37 +0800

    Add file_io/task_create_large_file.c, a task to create largre file with MARCRO: _FILE_OFFSET_BITS 64

---
+++ file_io/task_create_large_file.c
@@ -0,0 +1,32 @@
+/*************************************************************************\
+ Write large file IO with MACRO_FILE_OFFSET_BITS 64
+\*************************************************************************/
+
+
+
+#define _FILE_OFFSET_BITS 64
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "lap_base.h"
+
+int
+main(int argc, char *argv[])
+{
+    int fd;
+    off_t off;
+
+    if (argc != 3 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s pathname offset\n", argv[0]);
+
+    fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        errExit("Fail to open file.");
+
+    off = atoll(argv[2]);
+    if (lseek(fd, off, SEEK_SET) == -1)
+        errExit("Fail to apply lseek");
+
+    if (write(fd, "test", 4) == -1)
+        errExit("write");
+    exit(EXIT_SUCCESS);
+}
