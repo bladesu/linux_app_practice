6b9dcb50d2f2de11cd8f8f1694f62d2b1a13cb22
BladeSu <bladesu@inquartik.com>
2020-05-06 18:26:22 +0800

    Modify task_longjmp2returned.c, add segmentation fault case.

--- process/task_longjmp2returned.c
+++ process/task_longjmp2returned.c
@@ -1,17 +1,27 @@
 /*
     Here I demo a really bad implementation with non-local goto with setjmp and longjmp.
     It should always avoid longjmp to a returned function. Here we can see a embarrassing
-    situation that variables in the previous returned function are accessed with wrong values.
+    situation 
+    case 1) that variables in the previous returned function are accessed with wrong values.
+    case 2) segmentation fault when return to setjmp.
 */
 #include <stdlib.h>
 #include <setjmp.h>
 #include <stdio.h>
-static jmp_buf env;
+static jmp_buf env1;
+static jmp_buf env2;
 
-void func1()
+void setJumpChild(jmp_buf env)
+{
+    setjmp(env);
+}
+void setJumpMain(jmp_buf env){
+   setJumpChild(env); 
+}
+void func1(jmp_buf env)
 {
     int i_obj = 1;
-    char * s_obj = "a";
+    char *s_obj = "a";
     switch (setjmp(env))
     {
     case 0:
@@ -27,21 +37,32 @@ void func1()
         printf("<int(initialized with 1):%d>\n", i_obj);
         printf("<string(initialized with 'a\\0'):%s>\n", s_obj);
         printf("===========================================\n");
-        printf("Immediately Exit\n");
-        exit(EXIT_FAILURE);
         break;
     }
 }
 
-void func2()
+void func2(jmp_buf env)
 {
-    longjmp(env,1);
+    longjmp(env, 1);
 }
 
-
+static int r = 0;
+void recursive()
+{
+    r++;
+    if (r > 10)
+        return;
+    printf("do recursive loop~\n");
+    recursive();
+}
 int main(void)
 {
-    func1();
-    func2();
+    // case 1: unexpected data value
+    func1(env1);
+    func2(env1);
+    // case 2: Segmentation fault
+    setJumpMain(env2);
+    recursive();
+    func2(env2);
     exit(EXIT_SUCCESS);
 }
